10a00fb57b8b3dca43620b05b8836b624441e11d
Dave <dave@d53dev.net>
2016-07-15 23:14:30 +0200

    Moved to CMake, removed protobuf, added csaopt-tidings dep

--- src/producer.cpp
+++ src/producer.cpp
@@ -6,33 +6,28 @@
  */
 
 
-#include "spdlog.h"
-#include "cgopt.pb.h"
+#include <spdlog/spdlog.h>
 #include <zmqpp/zmqpp.hpp>
 #include <string>
 #include <iostream>
 #include <uuid/uuid.h>
 #include <random>
 #include <unistd.h>
-#include <google/protobuf/text_format.h>
 
 
-auto console = spdlog::stdout_logger_mt("console");
 
-void debug_request(cgopt_request request);
-void debug_response(cgopt_response response);
+
 void generate_work(std::string host, int port);
-void log_proto_array(const google::protobuf::RepeatedField<double> & iter);
-
-int main(int argc, char *argv[]) {
-	generate_work("localhost",5555);
-}
+//
+//int main(int argc, char *argv[]) {
+//	generate_work("localhost",5555);
+//}
 
 void generate_work(std::string host, int port) {
 
   const std::string endpoint = "tcp://"+host+":"+std::to_string(port).c_str();
 
-  console->info() << "Client Startup!";
+//  console->info() << "Client Startup!";
 
   // initialize the 0MQ context
   zmqpp::context context;
@@ -42,7 +37,7 @@ void generate_work(std::string host, int port) {
   zmqpp::socket socket (context, type);
 
   // open the connection
-  console->info() << "Opening connection to " << endpoint << "...";
+//  console->info() << "Opening connection to " << endpoint << "...";
   socket.connect(endpoint);
 
   std::uniform_real_distribution<double> unif(0,10000);
@@ -52,27 +47,27 @@ void generate_work(std::string host, int port) {
 	  usleep(3000000); //1sec
 	  zmqpp::message req_message;
 	  // compose a message from a string and a number
-	  cgopt_request request;
+//	  cgopt_request request;
 
 	  char uuidBuff[36];
 	  uuid_t uuidGenerated;
 	  uuid_generate_random(uuidGenerated);
 	  uuid_unparse(uuidGenerated, uuidBuff);
 
-	  request.set_id(uuidBuff);
-	  request.set_type(request.PUT_WORK);
+//	  request.set_id(uuidBuff);
+//	  request.set_type(request.PUT_WORK);
 
 	  for(int i=0;i<10;++i){
-		  request.add_data(unif(re));
+//		  request.add_data(unif(re));
 	  }
 
 	  //memcpy(fMessage.mutable_samples()->mutable_data(),
       //&dData[0],
       //sizeof(double)*dData.size());
 
-	  log_proto_array(request.data());
+//	  log_proto_array(request.data());
 
-	  req_message << request.SerializeAsString();
+//	  req_message << request.SerializeAsString();
 	  socket.send(req_message);
 
 	  //get the ACK
@@ -82,36 +77,36 @@ void generate_work(std::string host, int port) {
 	  socket.receive(rep_message);
 	  rep_message >> serialized_req;
 
-	  cgopt_response response;
-	  response.ParseFromString(serialized_req);
-
-	  assert(request.id() == response.id());
-	  console->info() << "Received " << response.id()<<", "<< response.type() << " for request " << request.id();
-	  if(response.has_error()){
-		  console->info() << "error received: "<<response.error();
-	  }
+//	  cgopt_response response;
+//	  response.ParseFromString(serialized_req);
+//
+//	  assert(request.id() == response.id());
+//	  console->info() << "Received " << response.id()<<", "<< response.type() << " for request " << request.id();
+//	  if(response.has_error()){
+//		  console->info() << "error received: "<<response.error();
+//	  }
   }
 }
 
-void log_proto_array(const google::protobuf::RepeatedField<double> & iter) {
-	std::string s;
-	for (auto it = iter.begin(); it != iter.end(); ++it) {
-		s += std::to_string((double)*it);
-		s += ", ";
-	}
-	console->info() << "Repeatedfield: [" << s << "]";
-}
+//void log_proto_array(const google::protobuf::RepeatedField<double> & iter) {
+//	std::string s;
+//	for (auto it = iter.begin(); it != iter.end(); ++it) {
+//		s += std::to_string((double)*it);
+//		s += ", ";
+//	}
+//	console->info() << "Repeatedfield: [" << s << "]";
+//}
 
 
-void debug_request(cgopt_request request) {
-	std::string s;
-	google::protobuf::TextFormat::PrintToString(request, &s);
-	console->info() << "Request " << s;
-}
-
-void debug_response(cgopt_response response) {
-
-	std::string s;
-	google::protobuf::TextFormat::PrintToString(response, &s);
-	console->info() << "Response " << s;
-}
+//void debug_request(cgopt_request request) {
+//	std::string s;
+//	google::protobuf::TextFormat::PrintToString(request, &s);
+//	console->info() << "Request " << s;
+//}
+//
+//void debug_response(cgopt_response response) {
+//
+//	std::string s;
+//	google::protobuf::TextFormat::PrintToString(response, &s);
+//	console->info() << "Response " << s;
+//}
--- src/worker.cpp
+++ src/worker.cpp
@@ -1,17 +1,12 @@
-#include "spdlog.h"
-#include "cgopt.pb.h"
 #include <zmqpp/zmqpp.hpp>
 #include <string>
 #include <iostream>
 #include <uuid/uuid.h>
 #include <random>
 #include <unistd.h>
-#include <google/protobuf/text_format.h>
+#include <spdlog/spdlog.h>
 
-void debug_request(cgopt_request request);
-void debug_response(cgopt_response response);
 void process_work(std::string host, int port);
-void log_proto_array(const google::protobuf::RepeatedField<double> & iter);
 
 auto console = spdlog::stdout_logger_mt("console");
 
@@ -39,71 +34,71 @@ void process_work(std::string host, int port) {
 		usleep(2000000); //1sec
 		zmqpp::message req;
 		// compose a message from a string and a number
-		cgopt_request request;
-		request.set_id("0");
-		request.set_type(request.GET_WORK);
-
-		req << request.SerializeAsString();
-		socket.send(req);
-		console->info() << "Sent request";
-
-		req.reset_read_cursor();
-		console->info() << "Getting response response";
-		socket.receive(req);
-
-		console->info() << "Received response";
-
-		std::string serialized_res;
-
-		req >> serialized_res;
-
-		cgopt_response cgresp;
-		cgresp.ParseFromString(serialized_res);
-
-		//assert(request.id() != cgresp.id());
-		if (cgresp.type() == cgresp.WORK && !cgresp.has_error()) {
-			auto a = cgresp.data();
-
-			log_proto_array(cgresp.data());
-
-			double sum = std::accumulate(a.begin(), a.end(), 0.0);
-			double mean = sum / a.size();
-
-			double sq_sum = std::inner_product(a.begin(), a.end(), a.begin(),
-					0.0);
-			double stdev = std::sqrt(sq_sum / a.size() - mean * mean);
-
-			console->info() << "ID=" << cgresp.id() << "\nData Avg=" << mean
-					<< " StDev=" << stdev;
-		} else {
-			console->warn() << "Response " << cgresp.id() << " has error "
-					<< cgresp.error();
-		}
-
+//		cgopt_request request;
+//		request.set_id("0");
+//		request.set_type(request.GET_WORK);
+//
+//		req << request.SerializeAsString();
+//		socket.send(req);
+//		console->info() << "Sent request";
+//
+//		req.reset_read_cursor();
+//		console->info() << "Getting response response";
+//		socket.receive(req);
+//
+//		console->info() << "Received response";
+//
+//		std::string serialized_res;
+//
+//		req >> serialized_res;
+//
+//		cgopt_response cgresp;
+//		cgresp.ParseFromString(serialized_res);
+//
+//		//assert(request.id() != cgresp.id());
+//		if (cgresp.type() == cgresp.WORK && !cgresp.has_error()) {
+//			auto a = cgresp.data();
+//
+//			log_proto_array(cgresp.data());
+//
+//			double sum = std::accumulate(a.begin(), a.end(), 0.0);
+//			double mean = sum / a.size();
+//
+//			double sq_sum = std::inner_product(a.begin(), a.end(), a.begin(),
+//					0.0);
+//			double stdev = std::sqrt(sq_sum / a.size() - mean * mean);
+//
+//			console->info() << "ID=" << cgresp.id() << "\nData Avg=" << mean
+//					<< " StDev=" << stdev;
+//		} else {
+//			console->warn() << "Response " << cgresp.id() << " has error "
+//					<< cgresp.error();
+//		}
+//
 	}
 
 }
 
 
-void log_proto_array(const google::protobuf::RepeatedField<double> & iter) {
-	std::string s;
-	for (auto it = iter.begin(); it != iter.end(); ++it) {
-		s += std::to_string((double)*it);
-		s += ", ";
-	}
-	console->info() << "Repeatedfield: [" << s << "]";
-}
+//void log_proto_array(const google::protobuf::RepeatedField<double> & iter) {
+//	std::string s;
+//	for (auto it = iter.begin(); it != iter.end(); ++it) {
+//		s += std::to_string((double)*it);
+//		s += ", ";
+//	}
+//	console->info() << "Repeatedfield: [" << s << "]";
+//}
 
 
-void debug_request(cgopt_request request) {
-	std::string s;
-	google::protobuf::TextFormat::PrintToString(request, &s);
-	console->info() << "Request " << s;
-}
-
-void debug_response(cgopt_response response) {
-
-	std::string s;
-	google::protobuf::TextFormat::PrintToString(response, &s);
-	console->info() << "Response " << s;
-}
+//void debug_request(cgopt_request request) {
+//	std::string s;
+//	google::protobuf::TextFormat::PrintToString(request, &s);
+//	console->info() << "Request " << s;
+//}
+//
+//void debug_response(cgopt_response response) {
+//
+//	std::string s;
+//	google::protobuf::TextFormat::PrintToString(response, &s);
+//	console->info() << "Response " << s;
+//}
