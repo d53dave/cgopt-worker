159c197ea75f6697c7cb0c3a3974edc7c0969fce
Dave <dave@d53dev.net>
2016-11-20 00:44:32 +0100

    Added class that will contain relevant cuda code

---
+++ src/CUDA/OptRunner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dotdi on 20.11.16.
+//
+
+#include "OptRunner.h"
---
+++ src/CUDA/OptRunner.h
@@ -0,0 +1,14 @@
+//
+// Created by dotdi on 20.11.16.
+//
+
+#ifndef CSAOPT_WORKER_OPTRUNNER_H
+#define CSAOPT_WORKER_OPTRUNNER_H
+
+
+class OptRunner {
+
+};
+
+
+#endif //CSAOPT_WORKER_OPTRUNNER_H
