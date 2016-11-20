52ada117424b96898f0cd31b9452ae7ceba8553f
Dave <dave@d53dev.net>
2016-11-20 21:53:28 +0100

    Cuda files need to be .cu, added csaopt namespace

--- src/CUDA/OptRunner.h
+++ src/CUDA/OptRunner.h
@@ -2,13 +2,11 @@
 // Created by dotdi on 20.11.16.
 //
 
-#ifndef CSAOPT_WORKER_OPTRUNNER_H
-#define CSAOPT_WORKER_OPTRUNNER_H
+#pragma once
 
+namespace CSAOpt {
+    class OptRunner {
 
-class OptRunner {
+    };
+}
 
-};
-
-
-#endif //CSAOPT_WORKER_OPTRUNNER_H
