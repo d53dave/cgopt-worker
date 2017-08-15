b1d2f6223c6c061274770e05637c3720d9a361aa
Dave <dave@d53dev.net>
2017-08-15 21:13:02 +0200

    Removed obsolete files

--- src/msgqueueclient.hpp
+++
@@ -1,9 +0,0 @@
-#include <zmqpp/zmqpp.hpp>
-#include <zmq.h>
-#include <string>
-#include <spdlog/spdlog.h>
-
-#ifndef CGOPT_MSGQUEUE_CLIENT_HPP_
-#define CGOPT_MSGQUEUE_CLIENT_HPP_
-
-#endif //CGOPT_MSGQUEUE_CLIENT_HPP_
