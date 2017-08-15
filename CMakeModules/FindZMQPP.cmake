bc93b7817246e202ed52befbe1240740f9e2b6a8
Dave <dave@d53dev.net>
2017-08-15 21:47:34 +0200

    Copied over cmake modules from old csaopt-zmq

--- CMakeModules/FindZMQPP.cmake
+++ CMakeModules/FindZMQPP.cmake
@@ -1,26 +1,23 @@
 # This will look for the ZMQPP headers and libraries and make
 # appropriate variables available to CMake.
 
-find_path(
-        ZMQPP_INCLUDE_DIRS
-        NAMES zmqpp/zmqpp.hpp
-        HINTS ${ZMQPP_INCLUDE_DIRS}
-)
+include(LibFindMacros)
 
-find_library(
-        ZMQPP_LIBRARIES
-        NAMES zmqpp
-        HINTS ${ZMQPP_LIBRARY_DIRS}
-)
+# Use pkg-config to get hints about paths
+libfind_pkg_check_modules(zmq_PKGCONF libzmq)
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-        ZMQPP
-        DEFAULT_MSG
-        ZMQPP_LIBRARIES
-        ZMQPP_INCLUDE_DIRS
-)
+# Include dir
+find_path(ZMQ_INCLUDE_DIR
+        NAMES zmq.hpp
+        PATHS ${zmq_PKGCONF_INCLUDE_DIRS})
 
-if(ZMQPP_FOUND)
-    mark_as_advanced(ZMQPP_LIBRARIES ZMQPP_INCLUDE_DIRS)
-endif()
+# Finally the library itself
+find_library(ZMQ_LIBRARY
+        NAMES zmq
+        PATHS ${zmq_PKGCONF_INCLUDE_DIRS})
+
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+set(zmq_PROCESS_INCLUDES zmq_INCLUDE_DIR )
+set(zmq_PROCESS_LIBS zmq_LIBRARY )
+libfind_process(zmqpp)
