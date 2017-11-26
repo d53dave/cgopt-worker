9d3903ba9c82245830b6577ee7269d4fefb7f889
Dave <dave@d53dev.net>
2017-11-26 22:18:20 +0100

    Added test method, working on GPU

--- app/worker/cuda.template.py
+++ app/worker/cuda.template.py
@@ -1,4 +1,5 @@
 from numba import cuda
+import math
 
 @cuda.jit(device=True)
 $initialize
@@ -20,6 +21,7 @@ def simulated_annealing(max_steps, initial_temp, states):
     thread_id = cuda.grid(1)
     step = 0
     # TODO: generate random vector
+    random_values = (0.1, 0.2, 0.3)
     state = initialize(random_values)
     energy = evaluate(state)
     temperature = initial_temp
@@ -27,9 +29,9 @@ def simulated_annealing(max_steps, initial_temp, states):
         temperature = cool(temperature)
         new_state = generate_next(state, random_values)
         new_energy = evaluate(new_state)
-        if acceptance_func():
+        if acceptance_func(energy, new_energy, temperature):
             state = new_state
             energy = new_energy
         step += 1
 
-    states[thread_id] = (state, energy)
+    states[thread_id] = state
--- app/worker/modulegenerator.py
+++ app/worker/modulegenerator.py
@@ -1,46 +1,67 @@
 import os
+import sys
 import imp
+import numpy as np
 
 from string import Template
 
 from typing import Dict
 
-@contextmanager
-def temp_open(path, mode):
-    # if this fails there is nothing left to do anyways
-    file = open(path, mode)
-
-    try:
-        yield file
-    finally:
-        file.close()
-        os.remove(path)
 
 class ModuleGenerator():
     def __init__(self, **conf):
         self.template_file = conf['template_file']
+        print('template file = {}'.format(self.template_file))
 
     def _render_template(self, func_strings: Dict[str, str]) -> str:
-        opt_template: str = ''
         with open(self.template_file, 'r') as f:
-            tempopt_templatelate = f.read().replace('\n', '')
-        
-        return opt_template.template()
+            opt_template = f.read()
 
-    def _load_module(self, name: str) -> any:
-        f, filename, description = imp.find_module('cuda_opt')
-        cuda_opt = imp.load_module('cuda_opt', f, filename, description)
+            print('Template string = {}'.format(opt_template))
+            return Template(opt_template).substitute(initialize=func_strings['initialize'],
+                                                     evaluate=func_strings['evaluate'],
+                                                     generate_next=func_strings['generate_next'],
+                                                     cool=func_strings['cool'],
+                                                     acceptance_func=func_strings['acceptance_func'])
 
-        assert cuda_opt is not None
+    def _create_module(self, name: str, source: str) -> any:
+        module = imp.new_module(name)
+        print('Executing source:\n{}'.format(source))
+        exec(source, module.__dict__)
+        sys.modules[name] = module
+
+        return module
+
+    def cuda_module(self, func_strings: Dict[str, str]) -> any:
+        source = self._render_template(func_strings)
+
+        cuda_opt = self._create_module('cuda_opt', source)
 
         return cuda_opt
-        
-    def cuda_module(self, func_strings: Dict[str, str]) -> any:
-        file_content = self._render_template(func_strings)
-        
-        with temp_open('cuda_opt.py', 'w') as cuda_file:
-            cuda_file.write(file_content)
 
-            cuda_opt = self._load_module('cuda_opt')
 
-            return cuda_opt
+if __name__ == '__main__':
+    template_path = sys.argv[1]
+    print('Running modulegen with {}'.format(template_path))
+    gen = ModuleGenerator(template_file=template_path)
+
+    func_strings = {}
+    func_strings['cool'] = """def cool(old_temp): return old_temp * .97"""
+
+    func_strings['initialize'] = """def initialize(randoms): return (1, 2, 3)"""
+
+    func_strings['generate_next'] = \
+        """def generate_next(state, randoms): return (state[0] + 1, state[1] + 1, state[2] + 1)"""
+
+    func_strings['evaluate'] = \
+        """def evaluate(state): return state[0] / state[1] / state[2]"""
+
+    func_strings['acceptance_func'] = \
+        """def acceptance_func(e1, e2, temp): return math.exp(-(e2 - e1) / temp)"""
+
+    cuda = gen.cuda_module(func_strings)
+
+    print('Generated module: {} =\n{}'.format(cuda, dir(cuda)))
+    states = np.array([(0.0, 0.0, 0.0)] * 3)
+    cuda.simulated_annealing[1, 3](100, 100, states)
+    print(states)
