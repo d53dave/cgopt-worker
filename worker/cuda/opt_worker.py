1807e67fc930edc20f8003d734e06eb8be2e72f2
David Sere <dave@d53dev.net>
2018-08-13 21:32:55 +0200

    Refactored worker for Dramatiq and additional fields on the model

--- worker/cuda/opt_worker.py
+++ worker/cuda/opt_worker.py
@@ -6,6 +6,7 @@ import numpy as np
 import uuid
 
 from numba.cuda.random import create_xoroshiro128p_states
+from pyhocon import ConfigTree
 from typing import Dict, Any
 
 from .modulegenerator import ModuleGenerator
@@ -21,24 +22,44 @@ from .. import OptResult
 
 
 class OptimizationWorker():
-    def __init__(self, conf: Dict[str, Any]) -> None:
+
+    def __init__(self, conf: ConfigTree) -> None:
+        self.conf = conf['optimization']
         self.gen = ModuleGenerator(template_file=conf['cuda.template_path'])
-        self.cuda = None
+        self.opt_configuration = None
+        self.opt_module = None
         self.id = str(uuid.uuid4())
 
-    async def run(self, model: Dict[str, Any]):
+
+    def compile_model(self, model: Dict[str, Any]):
+        self.opt_configuration = self._extract_opt_configuration(model)
+        self.opt_module = self.gen.cuda_module(self.opt_configuration)
+
+    async def run(self, opt_params):
         try:
-            opt_configuration = self._extract_opt_configuration(model)
-            rng_states = create_xoroshiro128p_states(3, seed=1)
-            cuda = self.gen.cuda_module(opt_configuration)
+            assert self.opt_module is not None
 
-            dimensions: int = int(opt_configuration['dim'])
+            dimensions: int = int(self.opt_configuration['dim'])
+            precision = np.float64 if self.opt_configuration['precision'] == 'float64' else np.float32
+            rng_states = create_xoroshiro128p_states(dimensions, seed=1)
 
-            values = np.array([0.0] * dimensions, dtype=np.float32)
-            states = np.array([(0.0, 0.0, 0.0)] * 3, dtype=np.float32)  # TODO: fix this, how?
-            cuda.simulated_annealing[1, 3](100, 100, 3, rng_states, states)
+            values = np.array([0.0] * dimensions, dtype=precision)
+            states = np.array([self.opt_module.state_shape()] *
+                              dimensions, dtype=precision)
+
+            max_steps = opt_params.get(
+                'max_steps', self.conf['defaults.max_steps'])
+            initial_temp = opt_params.get(
+                'initial_temp', self.conf['defaults.initial_temp'])
+            blocks_per_grid = opt_params.get(
+                'blocks_per_grid', self.conf['defaults.blocks_per_grid'])
+            grids_per_block = opt_params.get(
+                'grids_per_block', self.conf['defaults.grids_per_block'])
+
+            self.opt_module.simulated_annealing[blocks_per_grid, grids_per_block](
+                max_steps, initial_temp, rng_states, states)
             
-            return OptResult(states, values, None)
+            return OptResult(values, states, None)
         except:
             type_, value_, traceback_ = sys.exc_info()
             return OptResult(None, None, (type_, value_, traceback.format_tb(traceback_)))
@@ -57,11 +78,13 @@ class OptimizationWorker():
 
         opt_configuration['precision'] = model['precision']
         opt_configuration['dim'] = str(model['dimensions'])
+        opt_configuration['globals'] = model['globals']
         opt_configuration['cool'] = model['functions']['cool']
         opt_configuration['initialize'] = model['functions']['initialize']
         opt_configuration['generate_next'] = model['functions']['generate_next']
         opt_configuration['evaluate'] = model['functions']['evaluate']
-        opt_configuration['acceptance_func'] = model['functions']['ce_func']
+        opt_configuration['acceptance_func'] = model['functions']['acceptance_func']
+        opt_configuration['state_shape'] = model['functions']['state_shape']
 
         return opt_configuration
         
