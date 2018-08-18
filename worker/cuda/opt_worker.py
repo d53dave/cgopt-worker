53c42b81e9115fcafa08c822f6fa6fd7fcb203ed
David Sere <dave@d53dev.net>
2018-08-18 13:18:25 +0200

    Refactored OptResult to proper Class with to_dict() for serialization

--- worker/cuda/opt_worker.py
+++ worker/cuda/opt_worker.py
@@ -7,19 +7,40 @@ import uuid
 
 from numba.cuda.random import create_xoroshiro128p_states
 from pyhocon import ConfigTree
-from typing import Dict, Any, Tuple, Type, NamedTuple
+from typing import Dict, Any, Tuple, Type, Optional
 
 from .modulegenerator import ModuleGenerator
 
+Failure = Optional[
+    Tuple[Optional[Type[BaseException]],
+          Optional[BaseException],
+          str]]
 
-class OptResult(NamedTuple):
-    values: np.ndarray
-    states: np.ndarray
-    failure: Tuple[Type[Exception], Exception, str]
+
+class OptResult():
+    def __init__(self, values: np.ndarray, states: np.ndarray, failure: Failure=None) -> None:
+        self.values: np.ndarray = values
+        self.states: np.ndarray = states
+        self.failure: Failure = failure
+
+    def to_dict(self):
+        data = {
+            'values': self.values,
+            'states': self.states
+        }
+
+        if self.failure is not None:
+            data['failure'] = {
+                'type': str(self.failure[0]),       # pylint: disable=E1136
+                'exception': str(self.failure[1]),  # pylint: disable=E1136
+                'message': self.failure[2]          # pylint: disable=E1136
+            }
+
+        return data
 
 # TODO: add this to the model documentation
-# Numba(like cuRAND) uses the Box - Muller transform 
-# <https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform> to generate 
+# Numba(like cuRAND) uses the Box - Muller transform
+# <https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform> to generate
 # normally distributed random numbers from a uniform generator. However,
 # Box - Muller generates pairs of random numbers, and the current implementation
 # only returns one of them. As a result, generating normally distributed values
@@ -31,16 +52,19 @@ class OptimizationWorker():
     def __init__(self, conf: ConfigTree) -> None:
         self.conf = conf['optimization']
         self.gen = ModuleGenerator(template_file=conf['cuda.template_path'])
-        self.opt_configuration = None
-        self.opt_module = None
+        self.opt_configuration: Dict[str, str] = {}
+        self.opt_module: object = None
         self.id = str(uuid.uuid4())
 
+    def compile_model(self, model: Dict[str, Any]) -> str:
+        try:
+            self.opt_configuration = self._extract_opt_configuration(model)
+            self.opt_module = self.gen.cuda_opt(self.opt_configuration)  # type: ignore
+            return 'model_deployed'
+        except Exception as e:
+            return str(e)
 
-    def compile_model(self, model: Dict[str, Any]):
-        self.opt_configuration = self._extract_opt_configuration(model)
-        self.opt_module = self.gen.cuda_module(self.opt_configuration)
-
-    async def run(self, opt_params):
+    def run(self, opt_params) -> OptResult:
         try:
             assert self.opt_module is not None
 
@@ -49,7 +73,7 @@ class OptimizationWorker():
             rng_states = create_xoroshiro128p_states(dimensions, seed=1)
 
             values = np.array([0.0] * dimensions, dtype=precision)
-            states = np.array([self.opt_module.state_shape()] *
+            states = np.array([self.opt_module.state_shape()] *  # type: ignore
                               dimensions, dtype=precision)
 
             max_steps = opt_params.get(
@@ -59,15 +83,15 @@ class OptimizationWorker():
             blocks_per_grid = opt_params.get(
                 'blocks_per_grid', self.conf['defaults.blocks_per_grid'])
             grids_per_block = opt_params.get(
-                'grids_per_block', self.conf['defaults.grids_per_block'])
+                'threads_per_block', self.conf['defaults.threads_per_block'])
 
-            self.opt_module.simulated_annealing[blocks_per_grid, grids_per_block](
+            self.opt_module.simulated_annealing[blocks_per_grid, grids_per_block](  # type: ignore
                 max_steps, initial_temp, rng_states, states)
-            
+
             return OptResult(values, states, None)
-        except:
+        except Exception:
             type_, value_, traceback_ = sys.exc_info()
-            return OptResult(None, None, (type_, value_, traceback.format_tb(traceback_)))
+            return OptResult(None, None, (type_, value_, str(traceback.format_tb(traceback_))))
 
     def _extract_opt_configuration(self, model: Dict[str, Any]) -> Dict[str, str]:
         opt_configuration = {}
@@ -92,4 +116,3 @@ class OptimizationWorker():
         opt_configuration['state_shape'] = model['functions']['state_shape']
 
         return opt_configuration
-        
