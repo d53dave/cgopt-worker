acf2b821c882cc2c24da5e66fd7ec52944ad6c64
David Sere <dave@d53dev.net>
2018-08-19 23:52:21 +0200

    Several fixes in core SA CUDA code

--- worker/cuda/cuda.template.py
+++ worker/cuda/cuda.template.py
@@ -1,45 +1,59 @@
+# pylint: skip-file
+# flake8: noqa
+
+
 import numpy
 import array
 import math
 import cmath
 
+from math import pi
 from numba import cuda, $precision
 from numba.cuda.random import $random_gen_type$precision
+
+from typing import MutableSequence, Sequence, Any, Tuple
 
 $globals
 
 
 @cuda.jit(device=True)
-$context
-
-
-@cuda.jit(device=True)
+# @cuda.jit
 $initialize
 
 
 @cuda.jit(device=True)
+# @cuda.jit
 $cool
 
 
 @cuda.jit(device=True)
+# @cuda.jit
 $generate_next
 
 
 @cuda.jit(device=True)
+# @cuda.jit
 $evaluate
 
 
 @cuda.jit(device=True)
+# @cuda.jit
 $acceptance_func
 
 
-$state_shape
+$empty_state
 
 
 @cuda.jit
-def simulated_annealing(max_steps, initial_temp, rands, states):
-    ctx = context()
-    thread_id = cuda.grid(1)
+def simulated_annealing(max_steps, initial_temp, rands, states, values):
+    tx = cuda.threadIdx.x
+    ty = cuda.blockIdx.x
+    bw = cuda.blockDim.x
+    thread_id = tx + ty * bw
+
+    if thread_id >= states.size:
+        return
+
     step = 0
     rand_gen_idx = 0
     random_values = cuda.local.array($dim, dtype=$precision)
@@ -48,18 +62,21 @@ def simulated_annealing(max_steps, initial_temp, rands, states):
         rand_gen_idx += 1
     rand_gen_idx = 0
 
-    state = initialize(states[thread_id], random_values)
+    state = states[thread_id]
+    initialize(state, random_values)
     energy = evaluate(state)
+
     temperature = initial_temp
     while(step < max_steps):
-        # Generate Rands
         while(rand_gen_idx < $dim):
             random_values[rand_gen_idx] = $random_gen_type$precision(rands, thread_id)
             rand_gen_idx += 1
         rand_gen_idx = 0
 
-        temperature = cool(temperature)
-        new_state = generate_next(state, random_values)
+        temperature = cool(initial_temp, temperature, step)
+
+        new_state = numpy.copy(state)
+        generate_next(state, new_state, random_values)
         new_energy = evaluate(new_state)
         if acceptance_func(energy, new_energy, temperature):
             state = new_state
@@ -67,3 +84,5 @@ def simulated_annealing(max_steps, initial_temp, rands, states):
         step += 1
 
     states[thread_id] = state
+    values[thread_id] = energy
+    cuda.syncthreads()
